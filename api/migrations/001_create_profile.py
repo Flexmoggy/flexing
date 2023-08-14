758d3ef1b40632ef5391cbbf3bd468a5d24736ac
Freddy <edima_k@hotmail.com>
2023-08-14 15:41:20 -0400

    updated requirements.txt, began backend development

--- api/migrations/001_create_profile.py
+++ api/migrations/001_create_profile.py
@@ -2,16 +2,16 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE user (
+        CREATE TABLE profile (
             id SERIAL PRIMARY KEY NOT NULL,
             username VARCHAR(50) NOT NULL,
-            picture_url VARCHAR(),
+            picture_url VARCHAR,
             first_name VARCHAR(50) NOT NULL,
             last_name VARCHAR(50) NOT NULL,
             email VARCHAR(100) UNIQUE NOT NULL,
             skills VARCHAR (1000),
             interests VARCHAR (1000),
-            bio TEXT (10000)
+            bio TEXT
         );
         """,
         # "Down" SQL statement
