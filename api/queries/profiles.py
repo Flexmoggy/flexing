1725ab93bb8fa2a2c713f49ad81dd296cd4f7eee
Freddy <edima_k@hotmail.com>
2023-08-14 18:23:23 -0400

    corrected migrations, added profile.py to queries/routers.

--- api/queries/profiles.py
+++ api/queries/profiles.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
 from typing import Optional
+from queries.pool import pool
+
+
+class Error(BaseModel):
+    message: str
 
 
 class ProfileIn(BaseModel):
@@ -11,3 +16,54 @@ class ProfileIn(BaseModel):
     skills: Optional[str]
     interests: Optional[str]
     bio: Optional[str]
+
+
+class ProfileOut(BaseModel):
+    id: int
+    username: str
+    picture_url: Optional[str]
+    first_name: str
+    last_name: str
+    email: str
+    skills: Optional[str]
+    interests: Optional[str]
+    bio: Optional[str]
+
+
+class ProfileRepository:
+    def create(self, profile: ProfileIn) -> ProfileOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    INSERT INTO profile
+                    (
+                        username,
+                        picture_url,
+                        first_name,
+                        last_name,
+                        email,
+                        skills,
+                        interests,
+                        bio
+                    )
+                    VALUES
+                        (%s, %s, %s, %s, %s, %s, %s, %s)
+                    RETURNING id;
+                    """,
+                    [
+                     profile.username,
+                     profile.picture_url,
+                     profile.first_name,
+                     profile.last_name,
+                     profile.email,
+                     profile.skills,
+                     profile.interests,
+                     profile.bio,
+                     ]
+                )
+
+                id = result.fetchone()[0]
+                data = profile.dict()
+                #return {"message": "Looks like something went wrong. Check the input Data and try again."}
+                return ProfileOut(id=id, **data)
--- api/routers/profiles.py
+++ api/routers/profiles.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter
-from queries.profiles import ProfileIn
+from fastapi import APIRouter, Depends, Response
+from typing import Union
+from queries.profiles import ProfileIn, ProfileRepository, ProfileOut, Error
 
 
 router = APIRouter()
 
 
-@router.post("/profile")
-def create_profile(profile: ProfileIn):
-    pass
+@router.post("/profile", response_model=Union[ProfileOut, Error])
+def create_profile(profile: ProfileIn,
+                   #response: Response,
+                   repo: ProfileRepository = Depends()):
+    #response.status_code = 400
+    return repo.create(profile)
